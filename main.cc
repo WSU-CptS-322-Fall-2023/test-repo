46087c084aa07e50fdb523e35adfae2c33fde884
Sakire Arslan Ay <sakire.arslanay@wsu.edu>
2023-09-29 11:21:51 -0700

    A

---
+++ main.cc
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int h(){
+    printf("function h6 - adding more");
+    print("more messages")
+}
+
+int  f(int i){
+    int j = 987;
+
+    printf("%d\n", i+j);
+    print("fixed the issue")
+    return (i+j);
+}
+
+void main(){
+    printf("Output : %d", 12);
+}
